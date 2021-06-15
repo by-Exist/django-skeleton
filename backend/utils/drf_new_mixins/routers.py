f7a9a7ff34a2964c034064e95036be6745261bc1
Jongbeom-Kwon <bolk9652@naver.com>
2021-06-15 21:53:51 +0900

    Add routers
    drf_new_mixins를 사용할 때 즉시 활용할 수 있는 Router들을 기본적으로 제공하기로 하였다.

--- backend/utils/drf_new_mixins/routers.py
+++ backend/utils/drf_new_mixins/routers.py
@@ -1,10 +1,11 @@
-from rest_framework.routers import (
-    Route,
-    DynamicRoute,
-)
+from rest_framework import routers
+from rest_framework.routers import Route, DynamicRoute
+from rest_framework_nested.routers import NestedMixin
 
-custom_method_routes = [
-    # List route.
+
+# Routes
+# =============================================================================
+new_mixin_routes = [
     Route(
         url=r"^{prefix}{trailing_slash}$",
         mapping={"get": "list", "post": "create"},
@@ -12,15 +13,12 @@ custom_method_routes = [
         detail=False,
         initkwargs={"suffix": "List"},
     ),
-    # Dynamically generated list routes. Generated using
-    # @action(detail=False) decorator on methods of the viewset.
     DynamicRoute(
         url=r"^{prefix}/{url_path}{trailing_slash}$",
         name="{basename}-{url_name}",
         detail=False,
         initkwargs={},
     ),
-    # Detail route.
     Route(
         url=r"^{prefix}/{lookup}{trailing_slash}$",
         mapping={
@@ -33,8 +31,6 @@ custom_method_routes = [
         detail=True,
         initkwargs={"suffix": "Instance"},
     ),
-    # Dynamically generated detail routes. Generated using
-    # @action(detail=True) decorator on methods of the viewset.
     DynamicRoute(
         url=r"^{prefix}/{lookup}/{url_path}{trailing_slash}$",
         name="{basename}-{url_name}",
@@ -42,3 +38,56 @@ custom_method_routes = [
         initkwargs={},
     ),
 ]
+
+new_mixin_singleton_routes = [
+    Route(
+        url=r"^{prefix}{trailing_slash}$",
+        mapping={"get": "retrieve", "put": "replace", "patch": "modify"},
+        name="{basename}-detail",
+        detail=True,
+        initkwargs={"suffix": "Instance"},
+    ),
+    DynamicRoute(
+        url=r"^{prefix}/{url_path}{trailing_slash}$",
+        name="{basename}-{url_name}",
+        detail=True,
+        initkwargs={},
+    ),
+]
+
+
+# Mixins
+# =============================================================================
+class NotAllowCreateDestroyMixin:
+    # TODO: 내일 할 작업.
+    pass
+
+
+# Routers
+# =============================================================================
+class SimpleRouter(routers.SimpleRouter):
+    routes = new_mixin_routes
+
+
+class DefaultRouter(routers.DefaultRouter):
+    routes = new_mixin_routes
+
+
+class NestedSimpleRouter(NestedMixin, routers.SimpleRouter):
+    routes = new_mixin_routes
+
+
+class NestedDefaultRouter(NestedMixin, routers.DefaultRouter):
+    routes = new_mixin_routes
+
+
+class NestedSingletonSimpleRouter(
+    NestedMixin, NotAllowCreateDestroyMixin, routers.SimpleRouter
+):
+    routes = new_mixin_singleton_routes
+
+
+class NestedSingletonDefaultRouter(
+    NestedMixin, NotAllowCreateDestroyMixin, routers.DefaultRouter
+):
+    routes = new_mixin_singleton_routes
