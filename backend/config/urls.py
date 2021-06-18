b1cc00bdaaa5b31c60a2a8644c97fedbe33bab83
Jongbeom-Kwon <bolk9652@naver.com>
2021-06-18 21:51:05 +0900

    Url prefix "/api/" remove

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -13,20 +12,19 @@ from api.urls import API_VERSION, urlpatterns as api_urls
 # Common urls
 # =============================================================================
 urlpatterns = [
-    # path("admin/", admin.site.urls),
-    path(f"api/<version:version>/", include(api_urls)),
+    path(f"<version:version>/", include(api_urls)),
     path(
-        "api/<version:version>/schema/",
+        "<version:version>/schema/",
         SpectacularAPIView.as_view(api_version=API_VERSION),
         name="schema",
     ),
     path(
-        "api/<version:version>/schema/swagger/",
+        "<version:version>/schema/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger",
     ),
     path(
-        "api/<version:version>/schema/redoc/",
+        "<version:version>/schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
