e3851b32993bb1258fbd2ac5cda551779f65037c
Jongbeom-Kwon <bolk9652@naver.com>
2021-06-28 18:55:44 +0900

    Remove util exceptions.py
    - drf의 exception을 활용하는 것으로도 모두 처리할 수 있어 views.py 파일을 제거하였다.

--- backend/config/settings/common/third_party.py
+++ backend/config/settings/common/third_party.py
@@ -7,7 +7,6 @@ from .django import *
 INSTALLED_APPS += ["rest_framework"]
 REST_FRAMEWORK = {}
 REST_FRAMEWORK["DEFAULT_FILTER_BACKENDS"] = []
-REST_FRAMEWORK["EXCEPTION_HANDLER"] = "utils.drf_custom.views.exception_handler"
 
 
 # Django Filter
@@ -34,7 +33,7 @@ SPECTACULAR_SETTINGS["SERVE_INCLUDE_SCHEMA"] = False
 # Django Cachalot
 # =============================================================================
 # https://django-cachalot.readthedocs.io/en/latest/quickstart.html#settings
-INSTALLED_APPS += ["cachalot"]
-CACHES["cachalot"] = env.cache("DJANGO_CACHALOT_CACHE_URL")
-CACHALOT_CACHE = "cachalot"
-CACHALOT_UNCACHABLE_TABLES = ["django_migrations"]
+# INSTALLED_APPS += ["cachalot"]
+# CACHES["cachalot"] = env.cache("DJANGO_CACHALOT_CACHE_URL")
+# CACHALOT_CACHE = "cachalot"
+# CACHALOT_UNCACHABLE_TABLES = ["django_migrations"]
--- backend/utils/drf_custom/exceptions.py
+++ backend/utils/drf_custom/exceptions.py
@@ -1,8 +1,15 @@
+from rest_framework import exceptions as rest_exceptions, status as rest_status
+
+
 # ValidateOnlyGenericViewSetMixin
-class PerformValidateOnly(Exception):
-    pass
+class PerformValidateOnly(rest_exceptions.APIException):
+    status_code = rest_status.HTTP_204_NO_CONTENT
+    default_detail = "perform validate only."
+    default_code = "validate_only"
 
 
 # ValidateOnlySerializerMixin
-class BlockedSideEffect(Exception):
-    pass
+class BlockedSideEffect(rest_exceptions.APIException):
+    status_code = rest_status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = "validate_only 동작 수행에서 부작용이 있는 동작은 금지됩니다."
+    default_code = "validate_only"
